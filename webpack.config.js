627c7c565e5e6093907360f50fdec6687a28e3dc
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2020-12-11 21:25:12 +0700

    Webpack has been configuration

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    1: './src/1.js',
+    2: './src/2.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public/bundle')
+  },
+
+  plugins: [new webpack.ProgressPlugin()],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader'
+    }]
+  },
+
+  optimization: {
+    minimizer: [new TerserPlugin()],
+
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: false
+    }
+  }
+}

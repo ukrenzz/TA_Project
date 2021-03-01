ef5afac64e84c43650992774d6bdcd73cdc6672b
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-01 16:10:59 +0700

    Change jetstream register design to template register design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,81 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts/auth')
 
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+@section('auth-content')
+<div class="container-fluid p-h-0 p-v-20 bg full-height d-flex" style="background-image: url('assets/images/others/login-3.png')">
+    <div class="d-flex flex-column justify-content-between w-100">
+        <div class="container d-flex h-100">
+            <div class="row align-items-center w-100">
+                <div class="col-md-7 col-lg-5 m-h-auto">
+                    <div class="card shadow-lg">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center justify-content-between m-b-30">
+                                <img class="img-fluid" alt="" src="assets/images/logo/logo.png">
+                                <h2 class="m-b-0">Sign Up</h2>
                             </div>
+                            @if ($errors->any())
+                                <div class="alert alert-danger mb-5 text-sm text-danger" role="alert">
+                                    <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
+                                    @foreach ($errors->all() as $error)
+                                        <div>{{ $error }}</div>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label class="font-weight-semibold" for="name">{{ __('Name') }}</label>
+                                    <input type="text" class="form-control" name="name" id="name" :value="old('name')" placeholder="Full Name">
+                                </div>
+                                <div class="form-group">
+                                    <label class="font-weight-semibold" for="email">{{ __('Email') }}</label>
+                                    <input type="email" class="form-control" name="email" id="email" :value="old('email')" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label class="font-weight-semibold" for="password">{{ __('Password') }}</label>
+                                    <input type="password" class="form-control" name="password" id="password" required autocomplete="new-password" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <label class="font-weight-semibold" for="confirmPassword">{{ __('Confirm Password') }}</label>
+                                    <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex align-items-center justify-content-between p-t-15">
+                                        @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                                            <div class="checkbox">
+                                                <input id="checkbox" type="checkbox" name="terms" id="terms">
+                                                <label for="checkbox">
+                                                    {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                                            'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms').'</a>',
+                                                            'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy').'</a>',
+                                                    ]) !!}
+                                                </label>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex align-items-center justify-content-between p-t-15">
+                                        <span class="text-muted"><a href="{{ route('login') }}" class="text-muted">Already registered?</a></span>
+                                        <button class="btn btn-primary" type="submit">{{ __('Register') }}</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </x-jet-label>
+                    </div>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+        <div class="d-none d-md-flex p-h-40 justify-content-between">
+            <span class="">© 2019 ThemeNate</span>
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a class="text-dark text-link" href="">Legal</a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="text-dark text-link" href="">Privacy</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection

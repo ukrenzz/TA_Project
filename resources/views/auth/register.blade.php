e2713a6e1324b4783bb96cf16e3ef5c79a87d1a2
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-01 19:13:34 +0700

    Add name for name and email input textbox

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,11 +24,11 @@
                                 @csrf
                                 <div class="form-group">
                                     <label class="font-weight-semibold" for="name">{{ __('Name') }}</label>
-                                    <input type="text" class="form-control" name="name" id="name" :value="old('name')" placeholder="Full Name">
+                                    <input type="text" class="form-control" name="name" id="name" :value="old('name')" placeholder="Full Name" required autofocus autocomplete="name">
                                 </div>
                                 <div class="form-group">
                                     <label class="font-weight-semibold" for="email">{{ __('Email') }}</label>
-                                    <input type="email" class="form-control" name="email" id="email" :value="old('email')" placeholder="Email">
+                                    <input type="email" class="form-control" name="email" id="email" :value="old('email')" placeholder="Email" required>
                                 </div>
                                 <div class="form-group">
                                     <label class="font-weight-semibold" for="password">{{ __('Password') }}</label>

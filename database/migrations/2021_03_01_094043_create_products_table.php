f1d7c66fda7141872805f4c86e594a710c87cf12
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-01 19:14:40 +0700

    Create products migration

---
+++ database/migrations/2021_03_01_094043_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('unit', 20);
+            $table->foreignId('user_id');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

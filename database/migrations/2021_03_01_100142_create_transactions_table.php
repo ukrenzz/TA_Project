4469fac33a8dfcc851aef5cc3c5af6865db75a4e
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-01 19:15:58 +0700

    Create transactions migration

---
+++ database/migrations/2021_03_01_100142_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('ref', 100);
+            $table->foreignId('user_id')->constrained('users');
+            $table->smallInteger('ppn');
+            $table->smallInteger('status');
+            $table->bigInteger('discount');
+            $table->bigInteger('shipping_cost');
+            $table->string('payment_method', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}

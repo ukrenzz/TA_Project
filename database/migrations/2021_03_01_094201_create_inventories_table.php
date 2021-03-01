a522b4263383d277594a56814e99cb7d0ac9deb6
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-01 19:14:53 +0700

    Create inventories migration

---
+++ database/migrations/2021_03_01_094201_create_inventories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->foreignId('product_id')->constrained('products');
+            $table->integer('stock');
+            $table->bigInteger('price');
+            $table->bigInteger('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}

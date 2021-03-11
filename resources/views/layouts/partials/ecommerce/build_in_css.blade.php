cb36983e0313fdf0290a54f137a48d9d349fb712
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-03-11 19:10:44 +0700

    create partials page for e-commerce template

---
+++ resources/views/layouts/partials/ecommerce/build_in_css.blade.php
@@ -0,0 +1,13 @@
+<!-- Favicons-->
+<link rel="shortcut icon" href="assets/ecommerce/img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" type="image/x-icon" href="assets/ecommerce/img/apple-touch-icon-57x57-precomposed.png">
+<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="assets/ecommerce/img/apple-touch-icon-72x72-precomposed.png">
+<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="assets/ecommerce/img/apple-touch-icon-114x114-precomposed.png">
+<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="assets/ecommerce/img/apple-touch-icon-144x144-precomposed.png">
+
+<!-- GOOGLE WEB FONT -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+
+<!-- BASE CSS -->
+<link href="assets/ecommerce/css/bootstrap.custom.min.css" rel="stylesheet">
+<link href="assets/ecommerce/css/style.css" rel="stylesheet">
---
+++ resources/views/layouts/partials/ecommerce/build_in_script.blade.php
@@ -0,0 +1,3 @@
+<!-- COMMON SCRIPTS -->
+<script src="assets/ecommerce/js/common_scripts.min.js"></script>
+<script src="assets/ecommerce/js/main.js"></script>

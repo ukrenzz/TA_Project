7458fdec16b86803e19a48ec908aee77c7d2d76b
yienjesslyn <yienjesslyn@gmail.com>
2021-06-14 14:29:08 +0700

    feat : Extract Color Descriptor (in Product Controller)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,20 @@ use App\Http\Controllers\CategoryController;
 use App\Models\Product;
 use Storage;
 
+class colorInfo
+{
+  public $mean_IR;
+  public $mean_IG;
+  public $mean_IB;
+
+  public $lt_IR;
+  public $ht_IR;
+  public $lt_IG;
+  public $ht_IG;
+  public $lt_IB;
+  public $ht_IB;
+}
+
 class ProductController extends Controller
 {
   function index()
@@ -52,7 +66,7 @@ class ProductController extends Controller
   {
     $this->validation($req);
     // dd($req);
-    try{
+    try {
       Product::create([
         'name'        => $req->name,
         'brand'       => $req->brand,
@@ -68,8 +82,7 @@ class ProductController extends Controller
       ]);
 
       return response()->json(['success' => true]);
-    }
-    catch(Exception $ex) {
+    } catch (Exception $ex) {
       return response()->json(['success' => false, 'msg' => $ex->getMessage()]);
     }
     // return view('ecommerce.categories');
@@ -96,8 +109,67 @@ class ProductController extends Controller
       'description' => ['required', 'string'],
       'color'       => ['required', 'string'],
       'status'      => ['required', 'string'],
-      'discount'    => ['nullable', 'integer','digits_between:0,100'],
+      'discount'    => ['nullable', 'integer', 'digits_between:0,100'],
       // 'images.*' => 'mimes:jpg,png,jpeg,gif,svg'
     ]);
+  }
+
+  // -------------------------
+  // CTEBIR Algo 
+
+
+
+  function colorDescriptor($img)
+  {
+    // Color Descriptor
+    // Imagick untuk dapat Mean dan STDV 
+    $img = new Imagick('D:\New Neko Code\TA_Project\public\assets\image\Godfrey Gao.jpg');
+
+    // Mean and Std method from Imagick
+    $IR = $img->getImageChannelMean(1); // Red
+    $IG = $img->getImageChannelMean(4); // Green
+    $IB = $img->getImageChannelMean(2); // Blue 
+
+    // Mean, perlu direturn untuk citra dari DB
+    $mean_IR =  $IR["mean"];
+    $mean_IG =  $IG["mean"];
+    $mean_IB =  $IB["mean"];
+
+    // LT dan HT untuk ketiga channel warna RGB
+    $lt_IR =  $mean_IR - $IR["standardDeviation"];
+    $ht_IR =  $mean_IR + $IR["standardDeviation"];
+
+    $lt_IG =  $mean_IG - $IG["standardDeviation"];
+    $ht_IG =  $mean_IG + $IG["standardDeviation"];
+
+    $lt_IB =  $mean_IB - $IB["standardDeviation"];
+    $ht_IB =  $mean_IB + $IB["standardDeviation"];
+
+    $colorObj = new colorInfo();
+
+    $colorObj->mean_IR = $mean_IR;
+    $colorObj->mean_IG = $mean_IG;
+    $colorObj->mean_IB = $mean_IB;
+
+    $colorObj->lt_IR = $lt_IR;
+    $colorObj->ht_IR = $ht_IR;
+    $colorObj->lt_IG = $lt_IG;
+    $colorObj->ht_IG = $ht_IG;
+    $colorObj->lt_IB = $lt_IB;
+    $colorObj->ht_IB = $ht_IB;
+    dd($colorObj);
+
+
+    // TODO : Mau buat jadi object, baru bisa di return  
+    // return ($IR, $IG, $IB, $lt_IR, $ht_IR, $lt_IG, $ht_IG, $lt_IB, $ht_IB);
+
+    // TODO : mau dicoba, bagaimana cara akses properti $colorObj.  
+    return $colorObj;
+
+    // Source : 
+    // https://www.php.net/manual/en/imagick.getimagechannelmean.php
+    // https://www.php.net/manual/en/imagick.constants.php#imagick.constants.channel
+    // https://www.geeksforgeeks.org/php-imagick-getimagechannelmean-function/
+
   }
 }

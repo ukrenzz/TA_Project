cec2901ab945abdadf448beda1dde5380546f7d7
yienjesslyn <yienjesslyn@gmail.com>
2021-06-25 21:52:30 +0700

    feat : wishlist

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wishlist;
+use App\Models\Category;
+
 use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
   function index()
   {
-    return view('ecommerce.wishlist');
+    $wishlists = Wishlist::join('users', 'wishlists.user_id', '=', 'users.id')
+      ->join('products', 'wishlists.product_id', '=', 'products.id')
+      ->select('users.name as user_name', 'products.name as product_name', 'products.price as price', 'wishlists.created_at as created_at', 'wishlists.updated_at as updated_at')
+      ->orderBy('wishlists.created_at', 'desc')->get();
+
+    $categories = Category::orderBy('name', 'asc')->get();
+
+    $data = (object)[
+      'wishlists' => $wishlists,
+      'categories'=> $categories,
+    ];
+    return view('ecommerce.wishlist', compact('data'));
   }
 
   function create()

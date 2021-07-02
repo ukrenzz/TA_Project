df6892a94bf05a9c5ead77a4b70be89c4aa2fac2
yienjesslyn <yienjesslyn@gmail.com>
2021-07-02 14:58:59 +0700

    fix: change filename, so migration is in order, and not conflict in foreign key

--- database/migrations/2021_03_01_195754_create_feedbacks_table.php
+++ database/migrations/2021_03_01_195754_create_feedbacks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRatingsTable extends Migration
+class CreateFeedbacksTable extends Migration
 {
     /**
      * Run the migrations.

f720497b183395ede4c0bf7e969d0d98d3cd5169
yienjesslyn <yienjesslyn@gmail.com>
2021-07-06 21:02:46 +0700

    feat : show feedback on detail product

--- resources/views/ecommerce/detail.blade.php
+++ resources/views/ecommerce/detail.blade.php
@@ -174,52 +174,18 @@
           <div id="collapse-B" class="collapse" role="tabpanel" aria-labelledby="heading-B">
             <div class="card-body">
               <div class="row justify-content-between">
+                @foreach($data->feedbacks as $feedback)
                 <div class="col-lg-6">
                   <div class="review_content">
                     <div class="clearfix add_bottom_10">
-                      <span class="rating"><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><em>5.0/5.0</em></span>
-                      <em>Published 54 minutes ago</em>
+                      <span class="rating"><i class="icon-star"></i><em>{{$feedback->rate}}/5</em></span>
+                      <em>{{$feedback->created_at}}</em>
                     </div>
-                    <h4>"Commpletely satisfied"</h4>
-                    <p>Eos tollit ancillae ea, lorem consulatu qui ne, eu eros eirmod scaevola sea. Et nec tantas accusamus salutatus, sit commodo veritus te, erat legere fabulas has ut. Rebum laudem cum ea, ius essent fuisset ut. Viderer petentium cu his.</p>
+                    <h4>{{$feedback->comment}}</h4>
                   </div>
                 </div>
-                <div class="col-lg-6">
-                  <div class="review_content">
-                    <div class="clearfix add_bottom_10">
-                      <span class="rating"><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star empty"></i><i class="icon-star empty"></i><em>4.0/5.0</em></span>
-                      <em>Published 1 day ago</em>
-                    </div>
-                    <h4>"Always the best"</h4>
-                    <p>Et nec tantas accusamus salutatus, sit commodo veritus te, erat legere fabulas has ut. Rebum laudem cum ea, ius essent fuisset ut. Viderer petentium cu his.</p>
-                  </div>
-                </div>
+                @endforeach
               </div>
-              <!-- /row -->
-              <div class="row justify-content-between">
-                <div class="col-lg-6">
-                  <div class="review_content">
-                    <div class="clearfix add_bottom_10">
-                      <span class="rating"><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star empty"></i><em>4.5/5.0</em></span>
-                      <em>Published 3 days ago</em>
-                    </div>
-                    <h4>"Outstanding"</h4>
-                    <p>Eos tollit ancillae ea, lorem consulatu qui ne, eu eros eirmod scaevola sea. Et nec tantas accusamus salutatus, sit commodo veritus te, erat legere fabulas has ut. Rebum laudem cum ea, ius essent fuisset ut. Viderer petentium cu his.</p>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="review_content">
-                    <div class="clearfix add_bottom_10">
-                      <span class="rating"><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><i class="icon-star"></i><em>5.0/5.0</em></span>
-                      <em>Published 4 days ago</em>
-                    </div>
-                    <h4>"Excellent"</h4>
-                    <p>Sit commodo veritus te, erat legere fabulas has ut. Rebum laudem cum ea, ius essent fuisset ut. Viderer petentium cu his.</p>
-                  </div>
-                </div>
-              </div>
-              <!-- /row -->
-              <p class="text-right"><a href="leave-review.html" class="btn_1">Leave a review</a></p>
             </div>
             <!-- /card-body -->
           </div>

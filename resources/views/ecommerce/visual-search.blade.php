0fd213002059c4a5a1c0b0f751a1ee1285007be6
yienjesslyn <yienjesslyn@gmail.com>
2021-07-06 17:07:46 +0700

    fix : text search and visual search page

--- resources/views/ecommerce/visual-search.blade.php
+++ resources/views/ecommerce/visual-search.blade.php
@@ -21,48 +21,31 @@
       <h1>Search Result</h1>
     </div>
     <div class="row">
-      <aside class="col-lg-3" id="sidebar_fixed">
-        <div class="filter_col">
-          <div class="inner_bt"><a href="#" class="open_filters"><i class="ti-close"></i></a></div>
-          <div class="filter_type version_2">
-            <h4><a href="#filter_1" data-toggle="collapse" class="opened">Categories</a></h4>
-            <div class="collapse show" id="filter_1">
-              <ul>
-                @foreach($data->categories as $category )
-                <li>
-                  <label class="container_check">{{$category->name}} <small>12</small>
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                  </label>
-                </li>
-                @endforeach
-              </ul>
-            </div>
-          </div>
-          <div class="buttons">
-            <a href="#0" class="btn_1">Filter</a> <a href="#0" class="btn_1 gray">Reset</a>
-          </div>
-        </div>
-      </aside>
       <!-- /col -->
-      <div class="col-lg-9">
+      <div class="col-lg-12">
         <!-- /toolbox -->
         <div class="row small-gutters">
-          <div class="col-6 col-md-4">
+          @foreach($data->products as $product)
+          <div class="col-6 col-md-3">
             <div class="grid_item">
-              <span class="ribbon off">-30%</span>
+              <span class="ribbon off">-{{$product->discount}}%</span>
               <figure>
-                <a href="{{ route('product.show', ['id' => 1], ) }}">
+                <a href="{{ route('product.show', ['id' => $product->id]) }}">
                   <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/1.jpg') }}" alt="">
                 </a>
                 <div data-countdown="2019/05/15" class="countdown"></div>
               </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air x Fear</h3>
+              <a href="{{ route('product.show',['id' => $product->id]) }}">
+                <h3>{{$product->name}}</h3>
               </a>
               <div class="price_box">
-                <span class="new_price">$48.00</span>
-                <span class="old_price">$60.00</span>
+                <span class="new_price">Rp <?php
+                                            $oldprice = $product->price;
+                                            $disc = $product->discount;
+                                            $new = ($oldprice * (100 - $disc)) / 100;
+                                            echo $new;
+                                            ?></span><span class="percentage">-{{$product->discount}}</span>
+                <span class="old_price">Rp {{$product->price}}</span>
               </div>
               <ul>
                 <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
@@ -71,194 +54,7 @@
             </div>
             <!-- /grid_item -->
           </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon off">-30%</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/2.jpg') }}" alt="">
-                </a>
-                <div data-countdown="2019/05/10" class="countdown"></div>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Okwahn II</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$90.00</span>
-                <span class="old_price">$170.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon off">-50%</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/3.jpg') }}" alt="">
-                </a>
-                <div data-countdown="2019/05/21" class="countdown"></div>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air Wildwood ACG</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$75.00</span>
-                <span class="old_price">$155.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon new">New</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/4.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor ACG React Terra</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$110.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon new">New</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/5.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air Zoom Alpha</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$140.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon new">New</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/6.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air Alpha</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$130.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon hot">Hot</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/7.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air 98</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$115.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon hot">Hot</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/8.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor Air 720</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$120.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
-          <!-- /col -->
-
-          <div class="col-6 col-md-4">
-            <div class="grid_item">
-              <span class="ribbon hot">Hot</span>
-              <figure>
-                <a href="{{ route('product.show', ['id' => 1]) }}">
-                  <img class="img-fluid lazy" src="{{ asset('ecommerce/img/products/product_placeholder_square_medium.jpg') }}" data-src="{{ asset('ecommerce/img/products/shoes/9.jpg') }}" alt="">
-                </a>
-              </figure>
-              <a href="{{ route('product.show', ['id' => 1]) }}">
-                <h3>Armor 720</h3>
-              </a>
-              <div class="price_box">
-                <span class="new_price">$100.00</span>
-              </div>
-              <ul>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to favorites"><i class="ti-heart"></i><span>Add to favorites</span></a></li>
-                <li><a href="#0" class="tooltip-1" data-toggle="tooltip" data-placement="left" title="Add to cart"><i class="ti-shopping-cart"></i><span>Add to cart</span></a></li>
-              </ul>
-            </div>
-            <!-- /grid_item -->
-          </div>
+          @endforeach
           <!-- /col -->
         </div>
         <!-- /row -->

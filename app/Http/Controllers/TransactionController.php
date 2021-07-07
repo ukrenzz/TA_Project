eda708f9f8703d4dc4c4c6934c3f4d95129ddce1
yienjesslyn <yienjesslyn@gmail.com>
2021-07-07 23:18:13 +0700

    feat : payment

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
+use App\Models\User;
+use App\Models\Cart;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 
@@ -13,10 +15,12 @@ class TransactionController extends Controller
   {
     $categories = Category::orderBy('name', 'asc')->get();
     $transactions = Transaction::orderBy('created_at', 'asc')->get();
+
     $data = (object)[
       'transactions' => $transactions,
       'categories' => $categories,
     ];
+
     return view('ecommerce.payment', compact('data'));
   }
   function order()
@@ -38,16 +42,17 @@ class TransactionController extends Controller
   function create()
   {
     $categories = Category::orderBy('name', 'asc')->get();
-    $transactions = Transaction::join('users', 'users.id', '=', 'transactions.user_id')
-      ->join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
-      ->join('products', 'products.id', '=', 'transaction_details.product_id')
-      ->select('transactions.id', 'ref', 'users.name as username', 'products.name as product_name', 'ppn', 'transactions.status as status', 'transactions.discount', 'shipping_cost', 'payment_method', 'transaction_details.quantity as quantity', 'transaction_details.price as price', 'transactions.created_at', 'transactions.updated_at')
-      ->orderBy('updated_at', 'desc')
-      ->get();
+    $carts = Cart::join('products', 'carts.product_id', '=', 'products.id')
+      ->select('products.id as product_id', 'products.name as product_name', 'quantity', 'products.price as price', 'carts.created_at as created_at', 'carts.updated_at as updated_at')
+      ->where('user_id', '=', Auth::id())
+      ->orderBy('created_at', 'desc')->get();
+    $user = User::where('id', '=', Auth::id())->get()->first();
     $data = (object)[
-      'transactions' => $transactions,
+      'carts' => $carts,
       'categories' => $categories,
+      'user' => $user,
     ];
+    // dd($data);
     return view('ecommerce.payment', compact('data'));
   }
   function store()

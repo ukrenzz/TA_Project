7a03c04fa66d9cd312df1ec784f6bdb4c1ff2988
yienjesslyn <yienjesslyn@gmail.com>
2021-07-15 23:05:16 +0700

    feat : dasbboard page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Transaction;
+use Illuminate\Support\Facades\DB;
 
 
 class AdminController extends Controller
@@ -18,11 +19,13 @@ class AdminController extends Controller
       ->take(8)
       ->get();
     $transactions = Transaction::orderBy('created_at', 'asc')->take(8)->get();
+    $users = DB::table('users')->orderBy('name', 'asc')->take(8)->get();
 
     $data = (object)[
       'categories' => $categories,
       'products' => $products,
       'transactions' => $transactions,
+      'users' => $users,
     ];
     return view('admin.dashboard', compact('data'));
   }

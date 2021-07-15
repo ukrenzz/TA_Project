bc8a5de8354307aee3b3012287388af77bf21782
yienjesslyn <yienjesslyn@gmail.com>
2021-07-15 23:05:04 +0700

    FEAT :  dashboard page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+
 
 class AdminController extends Controller
 {
-    function index()
-    {
-      return view('admin.dashboard');
-    }
+  function index()
+  {
+    $categories = Category::orderBy('name', 'asc')->take(8)->get();
+    $products = Product::join('categories', 'products.category_id', '=', 'categories.id')
+      ->select('products.name as product_name', 'categories.name as product_category', 'products.id as id', 'brand', 'unit', 'color', 'products.description as description', 'price', 'stock', 'discount', 'products.created_at', 'products.updated_at')
+      ->orderBy('product_name', 'asc')
+      ->take(8)
+      ->get();
+    $transactions = Transaction::orderBy('created_at', 'asc')->take(8)->get();
+
+    $data = (object)[
+      'categories' => $categories,
+      'products' => $products,
+      'transactions' => $transactions,
+    ];
+    return view('admin.dashboard', compact('data'));
+  }
 }

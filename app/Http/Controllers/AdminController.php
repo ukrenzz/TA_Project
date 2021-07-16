d0b9f4e02a7fc3446162c83494178fba7e99944c
yienjesslyn <yienjesslyn@gmail.com>
2021-07-16 13:37:54 +0700

    refactor : remove unused code ( products from db for admin page)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,17 +13,11 @@ class AdminController extends Controller
   function index()
   {
     $categories = Category::orderBy('name', 'asc')->take(8)->get();
-    $products = Product::join('categories', 'products.category_id', '=', 'categories.id')
-      ->select('products.name as product_name', 'categories.name as product_category', 'products.id as id', 'brand', 'unit', 'color', 'products.description as description', 'price', 'stock', 'discount', 'products.created_at', 'products.updated_at')
-      ->orderBy('product_name', 'asc')
-      ->take(8)
-      ->get();
     $transactions = Transaction::orderBy('created_at', 'asc')->take(8)->get();
     $users = DB::table('users')->orderBy('name', 'asc')->take(8)->get();
 
     $data = (object)[
       'categories' => $categories,
-      'products' => $products,
       'transactions' => $transactions,
       'users' => $users,
     ];

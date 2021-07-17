8ad541f075dd6dc9e8f6b76bc07a5c0dc016e40b
Richy Rotuahta Saragih <richy.rotuahta15@gmail.com>
2021-07-17 21:27:26 +0700

    [Update] Change status transaction in order

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['middleware' => 'App\Http\Middleware\Admin'], function () {
 
   Route::name('order.')->group(function () {
     Route::get('/manage/order', [OrderController::class, 'index'])->name('manage');
+    Route::get('/manage/order/detail/{id}', [OrderController::class, 'show'])->name('show');
     Route::put('/manage/order/update', [OrderController::class, 'update'])->name('update');
   });
 
